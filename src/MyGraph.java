2bdf2b579008a0ec09a491ccee1b4206edebdc5b
KasperSaint07 <123306320+KasperSaint07@users.noreply.github.com>
2025-05-12 22:04:23 +0500

    edit MyGraph

--- src/MyGraph.java
+++ src/MyGraph.java
@@ -3,6 +3,7 @@ import java.util.List;
 
 public class MyGraph<T> {
     private List<Vertex<T>> vertices = new ArrayList<>();
+    private boolean directed;
 
     public void addVertex(Vertex<T> vertex) {
         if (!vertices.contains(vertex)) {
@@ -13,4 +14,19 @@ public class MyGraph<T> {
     public List<Vertex<T>> getVertices() {
         return vertices;
     }
+
+
+    public MyGraph(boolean directed) {
+        this.directed = directed;
+    }
+
+    public void addEdge(Vertex<T> source, Vertex<T> destination, double weight) {
+        addVertex(source);
+        addVertex(destination);
+        source.addAdjacentVertex(destination, weight);
+        if (!directed) {
+            destination.addAdjacentVertex(source, weight);
+        }
+    }
+
 }
